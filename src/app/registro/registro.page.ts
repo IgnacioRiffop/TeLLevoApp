148a23ad5103c1d12bfc3c6d7fc38b26395c0a32
IgnacioRiffop <ig.riffo@duocuc.cl>
2023-09-10 23:37:50 -0300

    registro alert

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -8,12 +9,22 @@ import { Router } from '@angular/router';
 })
 export class RegistroPage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,  private toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
 
   login(){
+    this.router.navigate(['/login']);
+  }
+
+  async loginreg(){
+    let men = this.toastCtrl.create({
+      message: "Registro realizado correctamente!",
+      duration: 5000,
+      position: 'bottom'
+    });
+    (await men).present();
     this.router.navigate(['/login']);
   }
 }
