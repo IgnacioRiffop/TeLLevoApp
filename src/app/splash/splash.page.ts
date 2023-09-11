3e8353bdde9f0f7b260030785a59f84f7f6d98ba
IgnacioRiffop <ig.riffo@duocuc.cl>
2023-09-10 22:41:16 -0300

    splash

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.page.html',
   styleUrls: ['./splash.page.scss'],
 })
-export class SplashPage implements OnInit {
+export class SplashPage implements AfterViewInit {
+  @ViewChild('titulo') titulo!: ElementRef;
 
-  constructor() { }
+  constructor(
+    private router: Router, private aniCtrl: AnimationController
+  ) { }
+  ngAfterViewInit(): void {
+    const ani = this.aniCtrl.create()
+    .addElement(this.titulo.nativeElement)
+    .duration(1500)
+    .iterations(Infinity)
+    .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+    .fromTo('opacity', '1', '0.2');
+    ani.play();
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.router.navigate(['/login']);
+    },5000);
   }
 
 }
