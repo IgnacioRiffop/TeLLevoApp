7e89097deb38b08a69d43ec6841b3695e1056220
IgnacioRiffop <ig.riffo@duocuc.cl>
2023-09-11 03:01:23 -0300

    viaje en curso

--- src/app/nuevo-viaje/nuevo-viaje.page.ts
+++ src/app/nuevo-viaje/nuevo-viaje.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuevo-viaje',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoViajePage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  viajeencurso(){
+    this.router.navigate(['/viaje-en-curso']);
   }
 
 }
--- src/app/viaje-en-curso/viaje-en-curso.page.ts
+++ src/app/viaje-en-curso/viaje-en-curso.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-viaje-en-curso',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViajeEnCursoPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  inicio(){
+    this.router.navigate(['/tabs/tab1']);
+  }
 }
