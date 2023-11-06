82ef9446aa4bb0419069e691e4baa796a3a1795a
IgnacioRiffop <ig.riffo@duocuc.cl>
2023-11-06 00:17:24 -0300

    mensaje

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { User } from '../shared/user.class';
+
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   public isLogged: any = false;
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, private toastCtrl: ToastController, private router: Router) {
     afAuth.authState.subscribe( user => (this.isLogged = user));
   }
 
@@ -21,6 +24,12 @@ export class AuthService {
     );
   } catch(error){
     console.log('Error on Login user', error);
+    let men = this.toastCtrl.create({
+      message: "Usuario o contraseña invalidos!",
+      duration: 5000,
+      position: 'top'
+    });
+    (await men).present();
     return null;
   }
  }
