de446ad8398686dc94ee5367a5a98bc516ee944d
IgnacioRiffop <ig.riffo@duocuc.cl>
2023-11-06 01:15:48 -0300

    guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { IsAuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,7 +10,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule), canActivate: [IsAuthGuard]
   },
   
   {
@@ -18,15 +19,15 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'nuevo-viaje',
-    loadChildren: () => import('./nuevo-viaje/nuevo-viaje.module').then( m => m.NuevoViajePageModule)
+    loadChildren: () => import('./nuevo-viaje/nuevo-viaje.module').then( m => m.NuevoViajePageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'ganancias',
-    loadChildren: () => import('./ganancias/ganancias.module').then( m => m.GananciasPageModule)
+    loadChildren: () => import('./ganancias/ganancias.module').then( m => m.GananciasPageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'registro',
@@ -38,38 +39,39 @@ const routes: Routes = [
   },
   {
     path: 'pasajeros',
-    loadChildren: () => import('./pasajeros/pasajeros.module').then( m => m.PasajerosPageModule)
+    loadChildren: () => import('./pasajeros/pasajeros.module').then( m => m.PasajerosPageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'historial-ganancia',
-    loadChildren: () => import('./historial-ganancia/historial-ganancia.module').then( m => m.HistorialGananciaPageModule)
+    loadChildren: () => import('./historial-ganancia/historial-ganancia.module').then( m => m.HistorialGananciaPageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'viaje-en-curso',
-    loadChildren: () => import('./viaje-en-curso/viaje-en-curso.module').then( m => m.ViajeEnCursoPageModule)
+    loadChildren: () => import('./viaje-en-curso/viaje-en-curso.module').then( m => m.ViajeEnCursoPageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'home2',
-    loadChildren: () => import('./home2/home2.module').then( m => m.Home2PageModule)
+    loadChildren: () => import('./home2/home2.module').then( m => m.Home2PageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'cuenta2',
-    loadChildren: () => import('./cuenta2/cuenta2.module').then( m => m.Cuenta2PageModule)
+    loadChildren: () => import('./cuenta2/cuenta2.module').then( m => m.Cuenta2PageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'historial2',
-    loadChildren: () => import('./historial2/historial2.module').then( m => m.Historial2PageModule)
-  },  {
+    loadChildren: () => import('./historial2/historial2.module').then( m => m.Historial2PageModule), canActivate: [IsAuthGuard]
+  },
+  {
     path: 'buscar-viaje',
-    loadChildren: () => import('./buscar-viaje/buscar-viaje.module').then( m => m.BuscarViajePageModule)
+    loadChildren: () => import('./buscar-viaje/buscar-viaje.module').then( m => m.BuscarViajePageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'viaje-en-curso-p',
-    loadChildren: () => import('./viaje-en-curso-p/viaje-en-curso-p.module').then( m => m.ViajeEnCursoPPageModule)
+    loadChildren: () => import('./viaje-en-curso-p/viaje-en-curso-p.module').then( m => m.ViajeEnCursoPPageModule), canActivate: [IsAuthGuard]
   },
   {
     path: 'escoger-viaje',
-    loadChildren: () => import('./escoger-viaje/escoger-viaje.module').then( m => m.EscogerViajePageModule)
+    loadChildren: () => import('./escoger-viaje/escoger-viaje.module').then( m => m.EscogerViajePageModule), canActivate: [IsAuthGuard]
   }
 
 
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,27 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+ class AuthGuard {
+  constructor(private authSvc: AuthService,private router: Router) {}
+  
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): boolean {
+    if (this.authSvc.isLogged){
+      return true;
+    }
+    console.log('Accceso denegado!');
+    this.router.navigate(['/login']);
+    return false;
   }
-  
+}   
+
+  export const IsAuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean =>{
+  return inject(AuthGuard).canActivate(route,state);
 }
+
