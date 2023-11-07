fa5dbdc6857364cd2b268094e7bafcb3a7239639
da-espinosa <da.espinosa@duocuc.cl>
2023-11-07 02:30:39 -0300

    mapa p

--- src/app/viaje-en-curso-p/viaje-en-curso-p.page.ts
+++ src/app/viaje-en-curso-p/viaje-en-curso-p.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as L from "leaflet";
 
 @Component({
   selector: 'app-viaje-en-curso-p',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViajeEnCursoPPage implements OnInit {
 
+  leafletMap: any;
+  lat: number = 41.1533;
+  
+  lng: number = 20.1683;
+  
+  zoom: number = 8;
+
   constructor() { }
 
   ngOnInit() {
+    this.loadLeafletMap();
+  }
+
+  loadLeafletMap() {
+    console.log('OK');
+    this.leafletMap = new L.Map('mapId');
+    this.leafletMap.setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.leafletMap);
+    var redicon = L.icon({
+      iconUrl : './../../assets/img/marcador.png',
+      iconSize: [35, 30],
+    });
+    L.marker([51.5, -0.09], {icon: redicon}).addTo(this.leafletMap);
   }
 
 }
