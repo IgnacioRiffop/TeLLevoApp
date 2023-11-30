9e67698436a5601793b1ea0549f742f861804d11
IgnacioRiffop <ig.riffo@duocuc.cl>
2023-11-30 01:56:04 -0300

    listar datos cuenta pasajero

--- src/app/cuenta2/cuenta2.page.ts
+++ src/app/cuenta2/cuenta2.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { ServicioFirebaseService } from '../services/servicio-firebase.service';
 
 @Component({
   selector: 'app-cuenta2',
@@ -8,11 +10,55 @@ import { Router } from '@angular/router';
   styleUrls: ['./cuenta2.page.scss'],
 })
 export class Cuenta2Page implements OnInit {
+  arreglo = [];
+  arr = new Array();
+  email:string='';
 
-  constructor(private router:Router, private afAuth: AngularFireAuth) {}
+  constructor(private router:Router, private afAuth: AngularFireAuth,private authSvc: AuthService, private servFire: ServicioFirebaseService) {}
 
-  ngOnInit() {
+  uid: string;
+
+  async obtenerUID() {
+    try {
+      const uid = await this.authSvc.getLoggedUserId();
+      if (uid) {
+        this.uid = uid; // Asigna el UID a la variable uid de la página
+        console.log('UID del usuario logueado:', this.uid);
+      } else {
+        console.log('Ningún usuario logueado.');
+      }
+    } catch (error) {
+      console.error('Error al obtener el UID del usuario logueado', error);
+    }
   }
+
+  datosUsuario: any;
+
+  async ngOnInit() {
+    await this.obtenerUID();
+
+    this.servFire.getDatosUsuario(this.uid).subscribe((datos) => {
+      this.datosUsuario = datos;
+      console.log('Datos del usuario:', this.datosUsuario);
+
+      // Puedes realizar otras operaciones con los datos del usuario aquí
+    });
+
+    var data = localStorage.getItem('email');
+    console.log(data);
+    if(data!=null){
+      console.log("no es nulo");
+      this.arreglo=JSON.parse(data);
+      console.log(this.arreglo);
+      this.arr.push(this.arreglo);
+      this.arr.forEach(element => {
+        console.log(element.email);
+        this.email=element.email;
+      })
+    }
+
+  }
+
 
   onLogout() {
     this.afAuth.signOut();
